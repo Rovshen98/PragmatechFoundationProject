f19dc9066878c6d6a37c13f873d25be4a8adeee8
Rovshen98 <rovshens@bk.ru>
2021-07-11 23:04:47 +0400

    mobile responsive

---
+++ ProjectFrontEnd/portfolio/javascript/common.js
@@ -0,0 +1,28 @@
+window.addEventListener('load', () =>{
+    const wrapper= document.querySelector(".wrapper");
+    wrapper.classList.add("wrapper-finish")
+});
+
+
+let burger = document.querySelector(".burger");
+let menu= document.querySelector(".menu");
+
+burger.addEventListener("click", ()=>{
+    menu.classList.toggle("active")
+});
+
+let x=document.querySelector(".top-to");
+
+window.addEventListener("scroll",()=>{
+    if(document.body.scrollTop > 130 || document.documentElement.scrollTop > 130){
+        x.style.display="block";
+    }
+    else{
+       x.style.display="none";
+    }
+})
+
+function topfunction() {
+    document.body.scrollTop = 0; 
+    document.documentElement.scrollTop = 0; 
+  }
