716b44711b812041dce6f43714f2e637f87f4e64
Rovshen98 <rovshens@bk.ru>
2021-08-29 11:45:58 +0400

    jpg

--- Works/Flask/oxu.az/app.py
+++ Works/Flask/oxu.az/app.py
@@ -1,9 +1,18 @@
 from flask import Flask,render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import secure_filename
+from werkzeug.datastructures import  FileStorage
+
+import os
+
+
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///ted.db'
 db = SQLAlchemy(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SERCER_KEY'] = 't53424u6756432456u56546342'
+app.config['UPLOAD_FOLDER']= 'oxu.az/static/uploads'
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -12,34 +21,40 @@ class User(db.Model):
     time = db.Column(db.String(120))
     title = db.Column(db.String(120))
     content = db.Column(db.String(120))
+    file=db.Column(db.String(100))
 
 @app.route("/", methods=["GET","POST"])
 def createnews():
-    News=User.query.all()
+    
     if request.method=="POST":
-       
+        file = request.files['file']
+        filename=secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'],filename))
+      
         day=request.form["day"]
         month=request.form["month"]
         time=request.form["time"]
         title=request.form["title"]
         content=request.form["content"]
         
+        
         data = User(
             day=day,
             month=month,
             time=time,
             title=title,
-            content=content
+            content=content,
+            file=file
         )
-        db.session.add(User)
+        db.session.add(data)
         db.session.commit()
         return redirect("/index")
     return render_template("admin.html")
 
 @app.route("/index")
 def index():
-    
-    return render_template("admin.html")
+    News=User.query.all()
+    return render_template("index.html",blog=News)
     
 
 
